7bab72bf0e3830f1179c02cd69c66e1a968f61dd
Rafael Simão <rafael.simonx@gmail.com>
2023-01-29 17:49:56 -0300

    - Adicionando readme. - Voltando a versão do cluster kubernetes para a versão "1.24.8-do.0", devido a uma incompatibiliadde do recurso "PodSecurityPolicy". O release será analisado posteriormente.

---
+++ README.md
@@ -0,0 +1,5 @@
+## Variáveis de ambiente necessárias
+#### Adicionar o arquivo "terraform.tfvars" na raiz do projeto preenchendo as seguintes variáveis de ambiente:
+- do_token
+- ssh_key_name
+- region
--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ data "digitalocean_ssh_key" "ssh_key" {
 resource "digitalocean_kubernetes_cluster" "k8s" {
   name    = "k8s"
   region  = var.region
-  version = "1.25.4-do.0"
+  version = "1.24.8-do.0"
 
   node_pool {
     name       = "default"
